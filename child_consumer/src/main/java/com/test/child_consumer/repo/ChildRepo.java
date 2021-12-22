2c94d824cb6354d7fcb3d8485cc824a9916e8df4
hoang-10n <s3749795@rmit.edu.vn>
2021-12-22 15:29:15 +0700

    temporarily removed

--- child_consumer/src/main/java/com/test/child_consumer/repo/ChildRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.child_consumer.repo;
-
-import com.test.child_consumer.model.Child;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChildRepo extends JpaRepository<Child, Integer> {
-}
--- parent_consumer/src/main/java/com/test/parent_consumer/repo/ParentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.parent_consumer.repo;
-
-import com.test.parent_consumer.model.Parent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParentRepo extends JpaRepository<Parent, Integer> {
-}
